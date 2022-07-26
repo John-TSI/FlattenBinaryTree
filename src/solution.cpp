1e2dc639af19b076594dc1704c17fb72109ea148
John Roughley <jroughley@softwareinstitute.com>
2022-07-26 22:45:55 +0100

    tidied the while loop

--- src/solution.cpp
+++ src/solution.cpp
@@ -6,13 +6,12 @@ void Solution::flatten(TreeNode* root)
     static std::vector<TreeNode*> nodes;
     MakePreorderVec(root, nodes);
     auto it = nodes.begin();
-    while(it != nodes.end()-1)
+    while(it != nodes.end())
     {
         (*it)->left = nullptr;
-        (*it)->right = *(it+1);
+        (*it)->right = (it+1 == nodes.end()) ? nullptr : *(it+1);
         ++it;
     }
-    nodes.back()->left = nullptr; nodes.back()->right = nullptr; 
 }
 
 void Solution::MakePreorderVec(TreeNode* root, std::vector<TreeNode*>& nodes)
